1d783876cdccf154351ad8e46a914eaf4848229a
Saifudin Badani <87968258+saifudinbadani@users.noreply.github.com>
2021-08-05 20:44:32 +0800

    changing text area

--- app.js
+++ app.js
@@ -2,4 +2,10 @@
 // var userName = prompt("enter your name")
 // alert("welcome to minion world " + userName)
 
-var translateButton = querySelector(#button)
+var translateButton = document.querySelector("#button");
+var txtInput = document.querySelector("#textarea1")
+
+translateButton.addEventListener("click", function clicked(){
+     return console.log("clicked!!")
+})
+
