762c6f7aad4dbc8f1cf87d7893236345a8520b2c
Saifudin Badani <87968258+saifudinbadani@users.noreply.github.com>
2021-08-06 18:05:31 +0800

    added catch

--- app.js
+++ app.js
@@ -2,23 +2,45 @@
 // var userName = prompt("enter your name")
 // alert("welcome to minion world " + userName)
 
+
+
+
+// Declaring variables and constants
+
 var translateButton = document.querySelector("#button");
 var txtInput = document.querySelector("#textarea1")
 var translatedText = document.querySelector("#translatedText")
 var url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+//  added encoding
 const encodedUrl = encodeURI(url)
 
-function constructUrl(text){
+
+// created function that will attach text input with url
+function constructUrl(text) {
     return encodedUrl + "?text=" + text
 }
-function clicked(){
-    // translatedText.innerText = "leMinionYou: " + txtInput.value
-    var input = txtInput.value
-    fetch(constructUrl(input))
-    .then(response => response.json())
-    .then(json => console.log(json))
-    
+
+// error handling
+function errorHandler(error){
+    console.log("Error occured" + error)
+    console.log("Try again after sometime.")
 }
 
-translateButton.addEventListener("click", clicked)
+// created function for what to happen when translate button is clicked.
+function clicked() {
+    // taking input
+    var input = txtInput.value
 
+    // promise
+
+    fetch(constructUrl(input))
+        .then(response => response.json())
+        .then(json => console.log(json))
+        .catch(errorHandler)
+
+}
+
+
+
+// added callback function to click
+translateButton.addEventListener("click", clicked)
