884869212a762c0d897da1013dc24d0c9ea0382c
Saifudin Badani <87968258+saifudinbadani@users.noreply.github.com>
2021-08-06 17:40:55 +0800

    construct fetch call

--- app.js
+++ app.js
@@ -5,10 +5,22 @@
 var translateButton = document.querySelector("#button");
 var txtInput = document.querySelector("#textarea1")
 var translatedText = document.querySelector("#translatedText")
+var url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
 
+function constructUrl(){
+    var mainUrl = url + "?text=" + txtInput
+    return mainUrl
+}
 function clicked(){
-    translatedText.innerText = "leMinionYou: " + txtInput.value
+    // translatedText.innerText = "leMinionYou: " + txtInput.value
+    fetch(constructUrl())
+    .then(response => response.json())
+    .then(json => console.log(json))
     
 }
 translateButton.addEventListener("click", clicked)
 
+// https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json
+// fetch(url)
+// .then(response => response.json())
+// .then(json => console.log(json))
