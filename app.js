2dc2ee5b95b01f21cd68bc9b0e292d233594015b
Saifudin Badani <87968258+saifudinbadani@users.noreply.github.com>
2021-08-06 17:55:47 +0800

    Added encodedUri

--- app.js
+++ app.js
@@ -6,21 +6,19 @@ var translateButton = document.querySelector("#button");
 var txtInput = document.querySelector("#textarea1")
 var translatedText = document.querySelector("#translatedText")
 var url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+const encodedUrl = encodeURI(url)
 
-function constructUrl(){
-    var mainUrl = url + "?text=" + txtInput
-    return mainUrl
+function constructUrl(text){
+    return encodedUrl + "?text=" + text
 }
 function clicked(){
     // translatedText.innerText = "leMinionYou: " + txtInput.value
-    fetch(constructUrl())
+    var input = txtInput.value
+    fetch(constructUrl(input))
     .then(response => response.json())
     .then(json => console.log(json))
     
 }
+
 translateButton.addEventListener("click", clicked)
 
-// https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json
-// fetch(url)
-// .then(response => response.json())
-// .then(json => console.log(json))
